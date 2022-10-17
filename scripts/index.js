92e6a74ff53780c6a7e39a02275cfd92de73420f
Max Skorokhodov <skormaksim@mail.ru>
2022-10-17 11:55:40 +0300

    feat: занести в поля формы данные из профиля

--- scripts/index.js
+++ scripts/index.js
@@ -14,17 +14,37 @@ let formUserName = formElement.querySelector('.form__item_user_name');
 let formUserOccupation = formElement.querySelector('.form__item_user_occupation');
 let formSubmit = formElement.querySelector('.form__submit');
 
+// open popup
 profileEditButton.addEventListener('click', function() {
+  formUserName.value = profileUserName.textContent.trim();
+  formUserOccupation.value = profileUserOccupation.textContent.trim();
+
   popupElement.classList.add('popup_opened');
 });
 
+// close popup
 popupCloseButton.addEventListener('click', function() {
   popupElement.classList.remove('popup_opened');
 });
 
+// close popup by click on overlay
+popupElement.addEventListener('click', function(event) {
+  if (event.target === event.currentTarget) {
+    popupElement.classList.remove('popup_opened');
+  };
+});
+
+// form
 formSubmit.addEventListener('click', function(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-  profileUserName.textContent = formUserName.value;
-  profileUserOccupation.textContent = formUserOccupation.value;
+  if (formUserName.value !== '') {
+    profileUserName.textContent = formUserName.value;
+  }
+
+  if (formUserOccupation.value !== '') {
+    profileUserOccupation.textContent = formUserOccupation.value;
+  }
+
+  popupElement.classList.remove('popup_opened');
 });
